b8c17ddcfd15fddd9bc83e5521e1d755a926fba2
cira1234 <77906052+cira1234@users.noreply.github.com>
2023-03-25 22:04:01 +0700

    Update serverfail.js

--- serverfail.js
+++ serverfail.js
@@ -1,37 +1,7 @@
-// const express =require()
-// const mysql = require('mysql');
-
-
-// const app = express();
-
-
-// var con =mysql.createConnection({
-
-//     host: "localhost",
-//     user: "root",
-//     password: "",
-//     database: "member"
-// });
-
-// app.get("/read",async(req,res)=>{
-
-//     try{
-
-//         connection.query("select * from employee",(err,result)=>{
-
-//             if(err){
-//                 console.log(err);
-//                 return res.status(400).send();
-
-//             }
-//             res.status(200).json
-
-//         })
-
-//     }catch(err){
-//         console.log(err);
-//         return res.status(500).send();
-//     }
-
-// })
-
+app.get('/userser',function(req,res,next){
+    console.log("HI");
+        }
+    )
+    app.listen(5000,function(){
+    console.log('enable port 5000')
+})
